7121e0f1516fdbc273b84542605e219bf9f488df
Patryk <ciaboss@o2.pl>
2014-06-10 13:25:08 +0200

    finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+
+  before_create :create_remember_token
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -7,4 +9,20 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+   def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+
+  private
+
+  def create_remember_token
+   self.remember_token = User.digest(User.new_remember_token)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   
   resources :users
-
-
+  resources :sessions, only: [:new, :create, :destroy]
+  
   root  'static_pages#home'
+
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
